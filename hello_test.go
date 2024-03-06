a64554ebaefabb464b2f322106fddacd31700617
test <josephnestor71@gmail.com>
2024-03-06 07:09:44 +0300

    Adder commit

--- hello_test.go
+++ hello_test.go
@@ -9,9 +9,14 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 
 	})
-	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+	t.Run("Empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello(" ")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Holla, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
